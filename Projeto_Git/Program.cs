76521ac16b8d23b8d3cb22510d218f65059eae5d
Juraci Magalhaes <jurahein@gmail.com>
2020-06-04 21:43:24 -0300

    Adicionado uma pergunta e um for para mostrar quantos 'OI' deseja aparecer

--- Projeto_Git/Program.cs
+++ Projeto_Git/Program.cs
@@ -7,6 +7,14 @@ namespace Projeto_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Teste Git!");
+            Console.WriteLine("Quantos 'oi' você quer que apareça ? ");
+            
+            int p = int.Parse(Console.ReadLine());
+            Console.WriteLine("-------------------");
+            for (int i = 0; i < p; i++)
+            {
+                Console.WriteLine(i+1 + ") --> " + " oi");
+            }
         }
     }
 }
