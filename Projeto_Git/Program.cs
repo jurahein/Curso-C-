364c3f7595586e4ab0f8aa9a588fe90deb8b6906
Juraci Magalhaes <jurahein@gmail.com>
2020-06-04 21:27:33 -0300

    Projeto demonstrando como commitar no Git

---
+++ Projeto_Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste Git!");
+        }
+    }
+}
